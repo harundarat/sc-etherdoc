25dd0f069d7de4c0b83e5f2ce8d07cf4558e36a4
Harun Al Rasyid <harundarat@gmail.com>
2026-07-18 17:09:22 +0700

    test: cover deployment preflight validation

---
+++ test/NetworkConfig.t.sol
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/contracts/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
+import {NetworkConfigScript} from "../script/NetworkConfig.s.sol";
+
+contract ConfigMockRouter is IRouterClient {
+    bool private s_supported;
+
+    function setSupported(bool _supported) external {
+        s_supported = _supported;
+    }
+
+    function isChainSupported(uint64) external view returns (bool supported) {
+        return s_supported;
+    }
+
+    function getFee(uint64, Client.EVM2AnyMessage memory) external pure returns (uint256 fee) {
+        return 0;
+    }
+
+    function ccipSend(uint64, Client.EVM2AnyMessage calldata) external payable returns (bytes32 messageId) {
+        return bytes32(uint256(1));
+    }
+}
+
+contract ConfigCodeStub {}
+
+contract NetworkConfigHarness is NetworkConfigScript {
+    function loadNetwork(string memory _networkName) external view returns (NetworkConfig memory) {
+        return _loadNetwork(_networkName);
+    }
+
+    function validateCurrentNetwork(NetworkConfig memory _network, bool _requireFeeToken) external view {
+        _validateCurrentNetwork(_network, _requireFeeToken);
+    }
+
+    function validateLane(NetworkConfig memory _source, NetworkConfig memory _destination) external view {
+        _validateLane(_source, _destination);
+    }
+
+    function requireDeployment(address _account, string memory _network, string memory _contractRole) external pure {
+        _requireDeployment(_account, _network, _contractRole);
+    }
+
+    function requireGasLimit(NetworkConfig memory _network, uint256 _expectedGasLimit) external pure {
+        _requireGasLimit(_network, _expectedGasLimit);
+    }
+}
+
+contract NetworkConfigTest is Test {
+    NetworkConfigHarness private s_harness;
+    ConfigMockRouter private s_router;
+    ConfigCodeStub private s_linkToken;
+
+    function setUp() public {
+        s_harness = new NetworkConfigHarness();
+        s_router = new ConfigMockRouter();
+        s_linkToken = new ConfigCodeStub();
+    }
+
+    function test_loadsEachSupportedChainFromJson() external view {
+        NetworkConfigScript.NetworkConfig memory source = s_harness.loadNetwork("ethereumSepolia");
+        NetworkConfigScript.NetworkConfig memory destination = s_harness.loadNetwork("baseSepolia");
+
+        assertEq(source.name, "ethereumSepolia");
+        assertEq(source.chainId, 11_155_111);
+        assertEq(source.chainSelector, 16_015_286_601_757_825_753);
+        assertNotEq(source.router, address(0));
+        assertNotEq(source.linkToken, address(0));
+        assertEq(source.rpcAlias, "ethereum_sepolia");
+        assertEq(source.gasLimit, 500_000);
+        assertEq(uint8(source.feeMode), uint8(NetworkConfigScript.FeeMode.LINK));
+        assertEq(source.sender, address(0));
+
+        assertEq(destination.name, "baseSepolia");
+        assertEq(destination.chainId, 84_532);
+        assertEq(destination.chainSelector, 10_344_971_235_874_465_080);
+        assertNotEq(destination.router, address(0));
+        assertNotEq(destination.linkToken, address(0));
+        assertEq(destination.rpcAlias, "base_sepolia");
+        assertEq(destination.gasLimit, 500_000);
+        assertEq(destination.receiver, address(0));
+    }
+
+    function test_dryRunRejectsWrongRpcChain() external {
+        NetworkConfigScript.NetworkConfig memory network = _network("source");
+        vm.chainId(network.chainId + 1);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NetworkConfigScript.ChainIdMismatch.selector, network.name, network.chainId, block.chainid
+            )
+        );
+        s_harness.validateCurrentNetwork(network, true);
+    }
+
+    function test_dryRunRejectsRouterWithoutCode() external {
+        NetworkConfigScript.NetworkConfig memory network = _network("source");
+        network.router = address(0xBEEF);
+        vm.chainId(network.chainId);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NetworkConfigScript.ContractCodeMissing.selector, network.name, "router", network.router
+            )
+        );
+        s_harness.validateCurrentNetwork(network, true);
+    }
+
+    function test_dryRunRejectsUnsupportedLane() external {
+        NetworkConfigScript.NetworkConfig memory source = _network("source");
+        NetworkConfigScript.NetworkConfig memory destination = _network("destination");
+        vm.chainId(source.chainId);
+        s_router.setSupported(false);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NetworkConfigScript.DestinationChainUnsupported.selector,
+                source.name,
+                destination.name,
+                destination.chainSelector
+            )
+        );
+        s_harness.validateLane(source, destination);
+    }
+
+    function test_acceptsMatchingChainContractsAndSupportedLane() external {
+        NetworkConfigScript.NetworkConfig memory source = _network("source");
+        NetworkConfigScript.NetworkConfig memory destination = _network("destination");
+        vm.chainId(source.chainId);
+        s_router.setSupported(true);
+
+        s_harness.validateLane(source, destination);
+    }
+
+    function test_configureRejectsMissingDeploymentAddress() external {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NetworkConfigScript.DeploymentAddressMissing.selector, "baseSepolia", "EtherdocReceiver"
+            )
+        );
+        s_harness.requireDeployment(address(0), "baseSepolia", "EtherdocReceiver");
+    }
+
+    function test_configureRejectsGasLimitMismatch() external {
+        NetworkConfigScript.NetworkConfig memory destination = _network("destination");
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                NetworkConfigScript.GasLimitMismatch.selector, destination.name, destination.gasLimit, 400_000
+            )
+        );
+        s_harness.requireGasLimit(destination, 400_000);
+    }
+
+    function _network(string memory _name) private view returns (NetworkConfigScript.NetworkConfig memory network) {
+        network = NetworkConfigScript.NetworkConfig({
+            name: _name,
+            chainId: 12_345,
+            chainSelector: 99,
+            router: address(s_router),
+            linkToken: address(s_linkToken),
+            explorer: "https://explorer.example",
+            rpcAlias: "test",
+            sender: address(0),
+            receiver: address(0),
+            gasLimit: 500_000,
+            feeMode: NetworkConfigScript.FeeMode.LINK,
+            directoryVerifiedAt: 1
+        });
+    }
+}
