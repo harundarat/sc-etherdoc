ff0ad7a9563e11aef5dc309200407a6be70134f3
Harun Al Rasyid <harundarat@gmail.com>
2026-07-18 16:33:28 +0700

    test: cover asynchronous delivery lifecycle

---
+++ test/EtherdocLifecycle.t.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/contracts/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
+import {LinkToken} from "@chainlink/local/src/shared/LinkToken.sol";
+import {EtherdocSender} from "../src/EtherdocSender.sol";
+import {EtherdocReceiver} from "../src/EtherdocReceiver.sol";
+
+contract DeferredRouter is IRouterClient {
+    struct QueuedMessage {
+        address receiver;
+        address sender;
+        bytes data;
+        bool exists;
+    }
+
+    uint64 public constant SOURCE_CHAIN_SELECTOR = 99;
+    uint256 public constant FEE = 1 ether;
+
+    uint256 private s_nonce;
+    mapping(bytes32 messageId => QueuedMessage message) private s_messages;
+
+    function isChainSupported(uint64) external pure returns (bool supported) {
+        return true;
+    }
+
+    function getFee(uint64, Client.EVM2AnyMessage memory) external pure returns (uint256 fee) {
+        return FEE;
+    }
+
+    function ccipSend(uint64 _destinationChainSelector, Client.EVM2AnyMessage calldata _message)
+        external
+        payable
+        returns (bytes32 messageId)
+    {
+        s_nonce++;
+        messageId = keccak256(abi.encode(_destinationChainSelector, msg.sender, _message.receiver, s_nonce));
+        s_messages[messageId] = QueuedMessage({
+            receiver: abi.decode(_message.receiver, (address)), sender: msg.sender, data: _message.data, exists: true
+        });
+    }
+
+    function deliver(bytes32 _messageId) external {
+        QueuedMessage storage queuedMessage = s_messages[_messageId];
+        require(queuedMessage.exists, "message not queued");
+
+        Client.Any2EVMMessage memory message = Client.Any2EVMMessage({
+            messageId: _messageId,
+            sourceChainSelector: SOURCE_CHAIN_SELECTOR,
+            sender: abi.encode(queuedMessage.sender),
+            data: queuedMessage.data,
+            destTokenAmounts: new Client.EVMTokenAmount[](0)
+        });
+
+        EtherdocReceiver(queuedMessage.receiver).ccipReceive(message);
+    }
+}
+
+contract EtherdocLifecycleTest is Test {
+    uint64 private constant DESTINATION_A = 11;
+    uint64 private constant DESTINATION_B = 22;
+    string private constant DOCUMENT_CID = "ipfs://bafy-lifecycle";
+
+    DeferredRouter private s_router;
+    LinkToken private s_link;
+    EtherdocSender private s_sender;
+    EtherdocReceiver private s_receiverA;
+    EtherdocReceiver private s_receiverB;
+    bytes32 private s_documentId;
+
+    function setUp() public {
+        s_router = new DeferredRouter();
+        s_link = new LinkToken();
+        s_sender = new EtherdocSender(address(s_router), address(s_link));
+        s_receiverA = new EtherdocReceiver(address(s_router));
+        s_receiverB = new EtherdocReceiver(address(s_router));
+
+        assertTrue(s_link.transfer(address(s_sender), 100 ether));
+        s_sender.configureDestinationChain(DESTINATION_A, address(s_receiverA), true);
+        s_sender.configureDestinationChain(DESTINATION_B, address(s_receiverB), true);
+        s_documentId = s_sender.registerDocument(DOCUMENT_CID);
+    }
+
+    function test_pendingDispatchIsNotReportedAsReceived() external {
+        bytes32 messageId = s_sender.dispatchDocument(s_documentId, DESTINATION_A);
+
+        EtherdocSender.DocumentRecord memory document = s_sender.getDocument(s_documentId);
+        EtherdocSender.DispatchRecord memory dispatch = s_sender.getDispatch(s_documentId, DESTINATION_A);
+        EtherdocReceiver.ReceiptRecord memory receipt = s_receiverA.getReceipt(s_documentId);
+
+        assertEq(uint8(document.status), uint8(EtherdocSender.DocumentStatus.REGISTERED));
+        assertEq(dispatch.messageId, messageId);
+        assertEq(uint8(dispatch.status), uint8(EtherdocSender.DispatchStatus.DISPATCHED));
+        assertEq(receipt.messageId, bytes32(0));
+        assertEq(uint8(receipt.status), uint8(EtherdocReceiver.ReceiptStatus.NOT_RECEIVED));
+        assertFalse(s_receiverA.isDocumentReceived(s_documentId));
+    }
+
+    function test_failedDestinationExecutionCanRetrySameMessage() external {
+        s_receiverA.allowListSourceChain(s_router.SOURCE_CHAIN_SELECTOR(), true);
+        bytes32 messageId = s_sender.dispatchDocument(s_documentId, DESTINATION_A);
+
+        vm.expectRevert(EtherdocReceiver.SenderNotAllowlisted.selector);
+        s_router.deliver(messageId);
+
+        EtherdocSender.DispatchRecord memory dispatch = s_sender.getDispatch(s_documentId, DESTINATION_A);
+        EtherdocReceiver.ReceiptRecord memory failedReceipt = s_receiverA.getReceipt(s_documentId);
+        assertEq(dispatch.messageId, messageId);
+        assertEq(uint8(dispatch.status), uint8(EtherdocSender.DispatchStatus.DISPATCHED));
+        assertEq(uint8(failedReceipt.status), uint8(EtherdocReceiver.ReceiptStatus.NOT_RECEIVED));
+
+        s_receiverA.allowlistSender(address(s_sender), true);
+        vm.warp(block.timestamp + 15);
+        s_router.deliver(messageId);
+
+        EtherdocReceiver.ReceiptRecord memory retriedReceipt = s_receiverA.getReceipt(s_documentId);
+        assertEq(retriedReceipt.messageId, messageId);
+        assertEq(retriedReceipt.documentCID, DOCUMENT_CID);
+        assertEq(retriedReceipt.sourceChainSelector, s_router.SOURCE_CHAIN_SELECTOR());
+        assertEq(retriedReceipt.sender, address(s_sender));
+        assertEq(retriedReceipt.receivedAt, block.timestamp);
+        assertEq(uint8(retriedReceipt.status), uint8(EtherdocReceiver.ReceiptStatus.RECEIVED));
+    }
+
+    function test_workflowIsCompleteOnlyAfterEveryDestinationHasReceipt() external {
+        _allowReceiver(s_receiverA);
+        _allowReceiver(s_receiverB);
+
+        bytes32 messageIdA = s_sender.dispatchDocument(s_documentId, DESTINATION_A);
+        bytes32 messageIdB = s_sender.dispatchDocument(s_documentId, DESTINATION_B);
+
+        s_router.deliver(messageIdA);
+        assertTrue(s_receiverA.isDocumentReceived(s_documentId));
+        assertFalse(s_receiverB.isDocumentReceived(s_documentId));
+
+        s_router.deliver(messageIdB);
+
+        EtherdocReceiver.ReceiptRecord memory receiptA = s_receiverA.getReceipt(s_documentId);
+        EtherdocReceiver.ReceiptRecord memory receiptB = s_receiverB.getReceipt(s_documentId);
+        assertTrue(s_receiverA.isDocumentReceived(s_documentId));
+        assertTrue(s_receiverB.isDocumentReceived(s_documentId));
+        assertEq(receiptA.messageId, messageIdA);
+        assertEq(receiptB.messageId, messageIdB);
+    }
+
+    function _allowReceiver(EtherdocReceiver _receiver) private {
+        _receiver.allowListSourceChain(s_router.SOURCE_CHAIN_SELECTOR(), true);
+        _receiver.allowlistSender(address(s_sender), true);
+    }
+}
